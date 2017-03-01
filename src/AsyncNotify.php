d15d243659b606cd56d4015c1b764a7981b264ae
alex <739849601@qq.com>
2017-03-01 17:13:47 +0800

    add notify processor

---
+++ src/AsyncNotify.php
@@ -0,0 +1,32 @@
+<?php
+namespace EasyPay;
+
+class AsyncNotify extends Notify
+{
+    /**
+     * @var array
+     */
+    protected static $modes = [
+        'ali'     =>  \EasyPay\Notify\Ali\AsyncProcessor::class,
+        'wechat'  =>  \EasyPay\Notify\Wechat\AsyncProcessor::class,
+    ];
+
+    /**
+     * 注册处理异步回调函数
+     *
+     * @param callable $callback
+     * @return string
+     */
+    public static function handle($mode, callable $callback)
+    {
+        $notify = static::getProcessor($mode);
+
+        try {
+            return $notify->success(
+                call_user_func($callback, $notify->getNotify()) ?: 'OK'
+            );
+        } catch(\Exception $e) {
+            return $notify->fail($e);
+        }
+    }
+}
